c4532456f2415d4ef9dcd9d9cd86b22978b99892
it21241246 <it21241246@my.sliit.lk>
2023-03-18 18:58:07 +0530

    Mad project second commit

---
+++ app/src/main/java/com/example/mad2/Income.kt
@@ -0,0 +1,23 @@
+package com.example.mad2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.R
+
+
+class Income : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_income, container, false)
+    }
+
+
+}
--- app/src/main/java/com/example/mad2/MainActivity.kt
+++ app/src/main/java/com/example/mad2/MainActivity.kt
@@ -11,50 +11,51 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-        val homeunselect: ImageView = findViewById(R.id.homeunselect)
+
+        val home: ImageView = findViewById(R.id.homeunselect)
         val goals:ImageView = findViewById(R.id.goals)
         val add:ImageView = findViewById(R.id.add)
         val stats:ImageView = findViewById(R.id.stats)
-        val settings:ImageView = findViewById(R.id.addfragment)
+        val settings:ImageView = findViewById(R.id.settings)
 
         val fragmentadd = Fragment()
-        //val fragmentUser = UserFragment()
+        val fragmentHome = home()
         add.setOnClickListener {
             add.setImageResource(R.drawable.add)
             supportFragmentManager.beginTransaction().apply {
-                replace(R.id.fragmentContainerView,addfragment )
+                replace(R.id.fragmentContainerView3,fragmentadd)
                 commit()
             }
         }
-        goals.setOnClickListener {
-            goals.setImageResource(R.drawable.selected_target)
-
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.fragmentContainerView,addfragment )
-                commit()
-            }
-        }
-        add.setOnClickListener {
-            stats.setImageResource(R.drawable.selected_stats)
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.fragmentContainerView,addfragment )
-                commit()
-            }
-        }
-        settings.setOnClickListener {
-            add.setImageResource(R.drawable.selected_settings)
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.fragmentContainerView,addfragment )
-                commit()
-            }
-        }
-        homeunselect.setOnClickListener {
-            homeunselect.setImageResource(R.drawable.home)
+//        goals.setOnClickListener {
+//            goals.setImageResource(R.drawable.selected_target)
+//
+//            supportFragmentManager.beginTransaction().apply {
+//                //replace(R.id.fragmentContainerView,addfragment )
+//                commit()
+//            }
+//        }
+//        add.setOnClickListener {
+//            stats.setImageResource(R.drawable.selected_stats)
+//            supportFragmentManager.beginTransaction().apply {
+//               // replace(R.id.fragmentContainerView,addfragment )
+//                commit()
+//            }
+//        }
+//        settings.setOnClickListener {
+//            add.setImageResource(R.drawable.selected_settings)
+//            supportFragmentManager.beginTransaction().apply {
+//                //replace(R.id.fragmentContainerView,addfragment )
+//                commit()
+//            }
+//        }
+        home.setOnClickListener {
+            home.setImageResource(R.drawable.home)
             goals.setImageResource(R.drawable.target)
             stats.setImageResource(R.drawable.stats)
             settings.setImageResource(R.drawable.settings)
             supportFragmentManager.beginTransaction().apply {
-                replace(R.id.fragmentContainerView2, fragmentHome)
+                replace(R.id.fragmentContainerView3, fragmentHome)
                 commit()
             }
         }
